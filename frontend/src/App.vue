67df3f433990260b41ee39f3f2beb9cf6d3b523d
Chris Michael <chris.perez@upr.edu>
2019-04-26 16:50:39 -0400

    :sparkles: frontend and streaming folder has been added to repo - fixes #11

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import home from './components/home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    home
+  }
+}
+</script>
